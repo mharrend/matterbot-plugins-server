daf532bcb7ca4bb9e64dec58748bb32c09e239c0
Marco A. Harrendorf <marco.harrendorf@cern.ch>
2016-06-07 10:27:44 +0200

    Update wetter.py

--- wetter.py
+++ wetter.py
@@ -11,8 +11,8 @@ import pyowm
 @respond_to('WetterAktuellAnzeigen')
 @listen_to('WetterAktuellAnzeigen')
 def wetter_aktuell(message):
-    owm = pyowm.OWM('dc9b24b06636a7fd46717bc4edef9b89')
-    observation = owm.weather_at_place('Eggenstein-Leopoldshafen,de')
+	owm = pyowm.OWM('dc9b24b06636a7fd46717bc4edef9b89')
+   	observation = owm.weather_at_place('Eggenstein-Leopoldshafen,de')
 	w = observation.get_weather()
 	message.reply('Wetter von %s' % w.get_reference_time(timeformat='iso'))
 	temperature = w.get_temperature('celsius')
@@ -23,10 +23,10 @@ wetter_aktuell.__doc__ = u"Zeigt aktuelles Wetter für KIT CN an"
 @respond_to('WetterVorhersageAnzeigen')
 @listen_to('WetterVorhersageAnzeigen')
 def wetter_vorhersage(message):
-    owm = pyowm.OWM('dc9b24b06636a7fd46717bc4edef9b89')
+   	owm = pyowm.OWM('dc9b24b06636a7fd46717bc4edef9b89')
 	fc = owm.daily_forecast('Eggenstein-Leopoldshafen,de', limit=3)
 	f= fc.get_forecast()
-    message.reply('Wettervorhersage von %s' % f.get_reception_time(timeformat='iso'))
+   	message.reply('Wettervorhersage von %s' % f.get_reception_time(timeformat='iso'))
 	for weather in f:
 		temperature = weather.get_temperature('celsius')
 		message.reply('Vorhersage für {0}:\n Wetteraussicht {1}\n Temperaturverlauf morgens {2}°C, mittags {3}°C, abends {4}°C, nachts {5}°C'.format(
